1f9ef29898e388ecbf8679df177493432feba6ea
iay <septianadip7998@gmail.com>
2024-05-07 03:27:55 +0700

    penambahan winner

--- src/components/tictactoe/TicTacToe.jsx
+++ src/components/tictactoe/TicTacToe.jsx
@@ -41,38 +41,42 @@ const TicTacToe = () => {
   const checkWin = () => {
     if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
       won(data[2]);
-    } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== ""){
+    } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
       won(data[5]);
-    }else if (data[6] === data[7] && data[7] === data[8] && data[8] !== ""){
+    } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
       won(data[8]);
-    }else if (data[0] === data[3] && data[3]=== data[6] && data[6] !== ""){
+    } else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
       won(data[6]);
-    }else if (data[1] === data[4] && data[4] === data[7] && data[7] !== ""){
+    } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
       won(data[7]);
-    }else if (data[2] === data[5] && data[5] === data[8] && data[8] !== ""){
+    } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
       won(data[8]);
-    }else if (data[0] === data[4] && data[4] === data[8] && data[8] !== ""){
+    } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
       won(data[8]);
-    }else if (data[0] === data[1] && data[1] === data[2] && data[2] !== ""){
+    } else if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
       won(data[2]);
-    }else if (data[2] === data[4] && data[4] === data[6] && data[6] !== ""){
+    } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
       won(data[6]);
     }
   }
 
   const won = (winner) => {
     setLock(true);
-    if(winner==="x"){
+    if (winner === "x") {
       titleRef.current.innerHTML = `Congratulations: <img src=${cross_icon}> Win`
-    }else{
+    } else {
       titleRef.current.innerHTML = `Congratulations: <img src=${circle_icon}> Win`
     }
   }
 
-  const reset = () =>{
+  const reset = () => {
     setLock(false);
     data = ["", "", "", "", "", "", "", "", ""];
-    titleRef.current.innerHTML = 'Tic Tac Toe In <span>React</span>'
+    titleRef.current.innerHTML = 'Tic Tac Toe In <span>React</span>';
+    box_array.map((e) => {
+      e.current.innerHTML = "";
+      return null;
+    })
   }
 
   return (
@@ -95,7 +99,7 @@ const TicTacToe = () => {
           <div className="boxes" ref={box9} onClick={(e) => { toggle(e, 8) }}></div>
         </div>
       </div>
-      <button className="reset" onClick={()=>{reset()}}>Reset</button>
+      <button className="reset" onClick={() => { reset() }}>Reset</button>
     </div>
   )
 }
